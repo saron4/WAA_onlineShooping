e06a2212b24330d2973095ee0cb9302b42a60966
saron4 <saronfantahun9@gmail.com>
2020-05-02 14:10:25 -0500

    initial commit

---
+++ src/main/java/com/group3/onlineShooping/OnlineShoppingGroup3Application.java
@@ -0,0 +1,13 @@
+package com.group3.onlineShooping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShoppingGroup3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineShoppingGroup3Application.class, args);
+	}
+
+}
