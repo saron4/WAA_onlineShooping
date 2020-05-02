0ced0970c196688ffa77f27ce5cde75b6c9c16e1
saron4 <saronfantahun9@gmail.com>
2020-05-02 16:43:14 -0500

    create product, use and category domain

---
+++ src/main/java/com/group3/onlineShooping/domain/Category.java
@@ -0,0 +1,41 @@
+package com.group3.onlineShooping.domain;
+
+import java.util.Set;
+
+import javax.persistence.Entity;
+
+import com.group3.onlineShooping.domain.Product;
+
+@Entity
+public class Category {
+	
+	private Long categoryId;
+	
+	private String categoryName;
+	
+	private Prdoduct products;
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+
+	public Prdoduct getProducts() {
+		return products;
+	}
+
+	public void setProducts(Prdoduct products) {
+		this.products = products;
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
