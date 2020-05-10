efc1d5aacff2e7b9af66e8ed0d2c3603b9129826
Ghebrehiywet Fissaha <g.hiwetmit@gmail.com>
2020-05-10 14:55:20 -0500

    order history + order status updated

---
+++ src/main/java/com/group3/onlineShooping/domain/OrderStatus.java
@@ -0,0 +1,58 @@
+package com.group3.onlineShooping.domain;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public enum OrderStatus {
+    ORDERED(1),
+    PURCHASED(2),
+    SHIPPED(3),
+    TRANSIT(4),
+    DELIVERED(5);
+
+    Integer val;
+
+    OrderStatus() {
+
+    }
+
+    OrderStatus(Integer val) {
+        this.val = val;
+    }
+
+    public void setVal(Integer val) {
+        this.val = val;
+    }
+
+    public Integer getVal() {
+        return val;
+    }
+
+    private static OrderStatus[] list = OrderStatus.values();
+
+    public static List<String> getShippingStatus(OrderStatus start, OrderStatus end) {
+        if (end == null) {
+            end = EndOrderStatus();
+        }
+        OrderStatus finalEnd = end;
+        List<String> result = Arrays.stream(list)
+                .filter(shippingStatus -> shippingStatus.val > start.getVal() && shippingStatus.val <= finalEnd.getVal())
+                .map(x -> x.toString()).collect(Collectors.toList());
+        return result;
+    }
+
+    public static OrderStatus InitOrderStatus() {
+        Optional<OrderStatus> result = Arrays.stream(list)
+                .min((o1, o2) -> o1.getVal().compareTo(o2.getVal()));
+        return result.get();
+    }
+
+    public static OrderStatus EndOrderStatus() {
+        Optional<OrderStatus> result = Arrays.stream(list)
+                .max((o1, o2) -> o1.getVal().compareTo(o2.getVal()));
+        return result.get();
+    }
+}
---
+++ src/main/java/com/group3/onlineShooping/service/impl/OrderHistoryServiceImpl.java
@@ -0,0 +1,4 @@
+package com.group3.onlineShooping.service.impl;
+
+public class OrderHistoryServiceImpl {
+}
