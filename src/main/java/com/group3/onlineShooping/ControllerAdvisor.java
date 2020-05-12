5ec895d1c811effb63a9d73e571b3737067cade1
Ghebrehiywet Fissaha <g.hiwetmit@gmail.com>
2020-05-12 14:47:58 -0500

    ExceptionHandler added

--- src/main/java/com/group3/onlineShooping/ControllerAdvisor.java
+++ src/main/java/com/group3/onlineShooping/ControllerAdvisor.java
@@ -2,8 +2,13 @@ package com.group3.onlineShooping;
 
 import com.group3.onlineShooping.domain.Notification;
 import com.group3.onlineShooping.service.NotificationService;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import java.util.List;
 
@@ -20,4 +25,23 @@ public class ControllerAdvisor {
         List<Notification> notifications = notificationService.getAllUnSeen();
         return notifications;
     }
+
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    @ExceptionHandler(AccessDeniedException.class)
+    public String accessDeniedException(AccessDeniedException e) throws Exception {
+        return "/accessdenied";
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
+    public String badRequest(Exception e) throws Exception {
+        return "/pagenotfound";
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
+    public String exception(Exception e) throws Exception {
+        return "/error";
+    }
+
 }
